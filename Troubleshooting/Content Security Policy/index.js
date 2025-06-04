2091bdf57f9f735f83d3be1b45d6999ddd64296f
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-06-04 14:05:50 +0530

    951648: Updated CSP versions

--- Troubleshooting/Content Security Policy/index.js
+++ Troubleshooting/Content Security Policy/index.js
@@ -1,7 +1,7 @@
 // Initialize PDF Viewer component
 var pdfviewer = new ej.pdfviewer.PdfViewer({
     documentPath: "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf",
-    resourceUrl: "https://cdn.syncfusion.com/ej2/29.1.37/dist/ej2-pdfviewer-lib"
+    resourceUrl: "https://cdn.syncfusion.com/ej2/29.2.8/dist/ej2-pdfviewer-lib"
 });
 // To utilize the server-backed PDF Viewer, need to specify the service URL.
 // This can be done by including the serviceUrl: 'https://services.syncfusion.com/js/production/api/pdfviewer'
