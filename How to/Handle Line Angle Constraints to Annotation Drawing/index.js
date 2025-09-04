9760120a58e87ed053828c63b4d3292da5f7e2ca
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-09-04 18:18:46 +0530

    123: Updated Sample on How to Handle Line Angle Constraints to Annotation Drawing.

---
+++ How to/Handle Line Angle Constraints to Annotation Drawing/index.js
@@ -0,0 +1,9 @@
+var pdfviewer = new ej.pdfviewer.PdfViewer({
+  documentPath: 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
+  resourceUrl: "https://cdn.syncfusion.com/ej2/31.1.1/dist/ej2-pdfviewer-lib",
+  // To enable the annotation drawing options
+  annotationDrawingOptions : {enableLineAngleConstraints : true, restrictLineAngleTo: 90}
+});
+ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Print, ej.pdfviewer.Navigation, ej.pdfviewer.Toolbar,
+  ej.pdfviewer.Magnification, ej.pdfviewer.Annotation, ej.pdfviewer.FormDesigner, ej.pdfviewer.FormFields, ej.pdfviewer.PageOrganizer);
+  pdfviewer.appendTo('#PdfViewer');
