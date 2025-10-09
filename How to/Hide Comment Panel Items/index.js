16f385bb311711d738259990e4a19148a0c4dd6a
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-10-09 16:19:08 +0530

    942079: hide commenl panel Items

---
+++ How to/Hide Comment Panel Items/index.js
@@ -0,0 +1,18 @@
+var pdfviewer = new ej.pdfviewer.PdfViewer({
+    documentPath: 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
+    resourceUrl: 'https://cdn.syncfusion.com/ej2/27.1.48/dist/ej2-pdfviewer-lib',
+});
+ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Print, ej.pdfviewer.Navigation, ej.pdfviewer.Toolbar, ej.pdfviewer.Magnification, ej.pdfviewer.Annotation, ej.pdfviewer.FormDesigner, ej.pdfviewer.FormFields);
+pdfviewer.appendTo('#PdfViewer');
+
+   document.getElementById('openCommentPanel').addEventListener('click', ()=> {
+        pdfviewer.annotation.showCommentsPanel();
+    });
+
+    document.getElementById('closeCommentPanel').addEventListener('click', ()=> {
+        pdfviewer.viewerBase.navigationPane.closeCommentPanelContainer();
+    });
+
+    pdfviewer.documentLoad = function (args) {
+        pdfviewer.annotation.showCommentsPanel();
+    }
