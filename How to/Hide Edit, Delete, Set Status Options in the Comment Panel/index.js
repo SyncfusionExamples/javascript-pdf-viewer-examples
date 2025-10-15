2b02271837cb6d3b46e6222d38ae6ec5c8e35325
SF4524LogeshKumar <166115263+SF4524LogeshKumar@users.noreply.github.com>
2025-10-15 13:43:40 +0530

    942079: Updated comments index.js

--- How to/Hide Edit, Delete, Set Status Options in the Comment Panel/index.js
+++ How to/Hide Edit, Delete, Set Status Options in the Comment Panel/index.js
@@ -4,15 +4,15 @@ var pdfviewer = new ej.pdfviewer.PdfViewer({
 });
 ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Print, ej.pdfviewer.Navigation, ej.pdfviewer.Toolbar, ej.pdfviewer.Magnification, ej.pdfviewer.Annotation, ej.pdfviewer.FormDesigner, ej.pdfviewer.FormFields);
 pdfviewer.appendTo('#PdfViewer');
-
+      // Open Comment Panel
    document.getElementById('openCommentPanel').addEventListener('click', ()=> {
         pdfviewer.annotation.showCommentsPanel();
     });
-
+    // Close Comment Panel
     document.getElementById('closeCommentPanel').addEventListener('click', ()=> {
         pdfviewer.viewerBase.navigationPane.closeCommentPanelContainer();
     });
-
+     // Automatically open Comment Panel on document load
     pdfviewer.documentLoad = function (args) {
         pdfviewer.annotation.showCommentsPanel();
     }
