d948a2fb97124dd71a7d960d7179f5e2ae5f6bc1
RangarajanSF4077 <rangarajan.sf4077@gmail.com>
2025-11-12 17:52:10 +0530

    EJ2-991842-DigitalSignature - Addressed the feedback

--- How to/Secure Document Signing with PDF Digital Signature Support/client-side/app.js
+++ How to/Secure Document Signing with PDF Digital Signature Support/client-side/app.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ej.pdfviewer.PageOrganizer
   );
 
-  ej.base.registerLicense('Ix0oFS8QJAw9HSQvXkVhQlBad1RDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS3tSdkVrWHxccXZVQGlfVk91Xg==');
+  ej.base.registerLicense('ENTER YOUR LICENSE KEY HERE');
 
   // Helpers
   var createElement = ej.base.createElement;
